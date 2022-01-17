eeda59b3f1d17cf279ae7f0c0782973cad7ac98a
Zhiqi Yu <yzq1027@hotmail.com>
2022-01-17 17:55:47 -0500

    update frontend

--- riceexplorer/settings.py
+++ riceexplorer/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z-%^%)1_br96+k%+=g$$-hdh)npn!xct!cbczf+95wlab*t)7b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
